6c7f155415b2ed011c514268a67a16cc89d36375
Lázaro Avelino Santos de Castro <lazarocastro@gmail.com>
2017-10-22 02:04:14 -0200

    Resolve ex21 and test ex20

--- exercises/ex20.rb
+++ exercises/ex20.rb
@@ -15,15 +15,12 @@ end
 current_file = open(input_file)
 
 puts "First let's print the whole file:\n"
-
 print_all(current_file)
 
 puts "Now let's rewind, kind of like a tape."
-
 rewind(current_file)
 
 puts "Let's print three lines:"
-
 current_line = 1
 print_a_line(current_line, current_file)
 
