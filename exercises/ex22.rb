57cb6d18d031a095f115e01247628f9f3e37f077
Lázaro Avelino Santos de Castro <lazarocastro@gmail.com>
2017-10-23 23:09:16 -0200

    Create ex22 and modify ex21

---
+++ exercises/ex22.rb
@@ -0,0 +1,26 @@
+puts "Ex1 - First program."
+puts "We learned about \"puts\", and how to print a message on the screen."
+puts "." * 70
+
+puts "Ex2 - Comments and Pound Characters."
+puts "We learned how to use \"#\" for put a comment inside de code" # like this
+puts "." * 70
+
+puts "Ex3 - Numbers and Math"
+print "Here we learned about lots math symbols; and how to use \"\#\{ \}\" :"
+puts """
+  + plus
+  - minus
+  / slash
+  * asterisk
+  % percent
+  < less-than
+  > greater-than
+  <= less-than-equal
+  >= greater-than-equal
+"""
+puts "." * 70
+
+puts "Ex4 - Variables and Names"
+puts "We learned techniques to work with variables"
+puts "." * 70
