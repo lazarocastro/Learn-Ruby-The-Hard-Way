38494c0160d58db4d6cbb11262a790452d058a06
Lázaro Avelino Santos de Castro <lazarocastro@gmail.com>
2017-10-24 10:09:52 -0200

    Update Ex22

--- exercises/ex22.rb
+++ exercises/ex22.rb
@@ -7,7 +7,7 @@ puts "We learned how to use \"#\" for put a comment inside de code" # like this
 puts "." * 70
 
 puts "Ex3 - Numbers and Math"
-print "Here we learned about lots math symbols; and how to use \"\#\{ \}\" :"
+puts "Here we learned about lots math symbols; and how to use \"\#\{ \}\" :"
 puts """
   + plus
   - minus
@@ -23,4 +23,46 @@ puts "." * 70
 
 puts "Ex4 - Variables and Names"
 puts "We learned techniques to work with variables"
+puts "." * 70
+
+puts "Ex5, 6, 7, 8, 9 and 10 - It's about printing techniques"
+puts "We saw many exercices with:"
+puts """
+ \#\{ \}
+ \%\{ \}
+ \%\q\{ \}
+"""
+puts "." * 70
+
+puts "Ex11, 12 and 13 - Asking Questions"
+puts "We learned how to get what the user type"
+puts """
+  gets.chomp
+  gets.chomp.to_i
+  gets.chomp.to_f
+  ARGV <--------------- use to get many arguments
+  $stdin.gets.chomp <-- use when you want to ask the user during running program
+  ARGV.first <--------- use to get only one argument
+
+  What's the difference between ARGV and gets.chomp?
+
+  The difference has to do with where the user is required to
+  give input. If they give your script inputs on the command
+  line, then you use ARGV. If you want them to input using the
+  keyboard while the script is running, then use gets.chomp.
+"""
+puts "." * 70
+
+puts "Ex15, 16, 17 - Reading Files"
+puts "We learned techniques to work with read and write onto files "
+puts """
+  open() <----------- You can do 'w+', 'r+', and 'a+'. This will open the file in both read and write mode.
+  .close <----------- Closes the file. Like File->Save.. in your editor.
+  .read <------------ Reads the contents of the file. You can assign the result to a variable.
+  .readline <-------- Reads just one line of a text file.
+  .truncate <-------- Empties the file. Watch out if you care about the file.
+  .write('stuff') <-- Writes \"stuff\" to the file.
+  .seek(0) <--------- Move the read/write location to the beginning of the file.
+  File.exist?
+"""
 puts "." * 70
