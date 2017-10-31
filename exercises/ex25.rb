8cb1be0049b56cf4ec0583c9444d4693508bcc0c
Lázaro Avelino Santos de Castro <lazarocastro@gmail.com>
2017-10-31 13:19:44 -0200

    Resolve Ex26 and modify ex25

--- exercises/ex25.rb
+++ exercises/ex25.rb
@@ -40,7 +40,7 @@ module Ex25
   def Ex25.print_first_and_last_sorted(sentence)
     words = Ex25.sort_sentence(sentence)
     Ex25.print_first_word(words)
-    Ex25.print_last_word
+    Ex25.print_last_word(words)
   end
 
 end
