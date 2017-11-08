970bd736cf4350f62648f7be13072042aa6c1cf5
Lázaro Avelino Santos de Castro <lazarocastro@gmail.com>
2017-11-08 11:46:42 -0200

    Resolve ex32 and ex33; Update ex31

--- exercises/ex31.rb
+++ exercises/ex31.rb
@@ -34,5 +34,4 @@ elsif door == "2"
 
 else
   puts "You stumble around and fall on a knife and die. Good job!"
-
 end
---
+++ exercises/ex33.rb
@@ -0,0 +1,23 @@
+def counts(count)
+  i = 0
+  numbers = []
+  while i <= count
+    puts "At the top 'i' is #{i} "
+    numbers.push(i)
+  
+    i += 1
+    puts "Numbers now: ", numbers
+    puts "At the bottom 'i' is #{i}"
+  end
+  
+  puts "The numbers: "
+  
+  # Remember you can write this 2 other ways?
+  # numbers.each {|num| puts num}
+  
+  numbers.each do |num|
+    puts num
+  end
+end
+
+counts(10)
