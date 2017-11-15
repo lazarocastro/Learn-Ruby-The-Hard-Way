c875e4355e4c1cfd82ef3aa61f052fcfeb81aac0
Lázaro Avelino Santos de Castro <lazarocastro@gmail.com>
2017-11-14 23:29:52 -0200

    Exercice 39

---
+++ exercises/ex39.rb
@@ -0,0 +1,23 @@
+states = {
+  'Oregon' => 'OR',
+  'Florida' => 'FL',
+  'California' => 'CA',
+  'New York' => 'NY',
+  'Michigan' => 'MI'
+}
+
+cities = {
+  'CA' => 'San Francisco',
+  'MI' => 'Detroit',
+  'FL' => 'Jacksonville'
+}
+
+# add some more cities
+cities['NY'] = 'New York'
+cities['OR'] = 'Portland'
+
+# puts out some cities
+puts '--' * 10
+puts "NY State has: #{cities['NY']}"
+puts "OR State has: #{cities['OR']}"
+
